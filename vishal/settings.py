464115d418959974c82c6731a7cda792c53fd90f
Vishal Khandelwal <vishalkhandelwaal@gmail.com>
2020-05-09 15:56:50 +0530

    added mongo cluster

--- vishal/settings.py
+++ vishal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from pymodm import connect
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,9 @@ WSGI_APPLICATION = 'vishal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+connect(
+    'mongodb+srv://vishal:VishalAman12@cluster0-ybpdk.mongodb.net/test?retryWrites=true&w=majority'
+)
 
 
 # Password validation
